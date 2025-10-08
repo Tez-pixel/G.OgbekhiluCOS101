3efc5be7c5be0679bb279b2dcb8ba8463c8509c4
ALLY <godswill.ogbekhilu@pau.edu.ng>
2025-10-08 22:41:47 +0100

    My second commit

---
+++ week-2/practice_1.rs
@@ -0,0 +1,12 @@
+fn main() {
+	println!("Welcome to COS101!");
+
+	println!("The course learning outcome is as follows");
+
+	println!("1. Distinguish between computer and computer programming,/n
+			 2. Develop some teechniques in computer science,/n
+			 3. Understand the different areas of study in computer science,/n
+			 4. Conersant with applications of computer science./n
+			 5. Conversant with computer programming"); 
+
+}
