9addc83f866822559fdea6d7fe36a11cd4183c1b
ALLY <godswill.ogbekhilu@pau.edu.ng>
2025-11-05 17:16:15 +0100

    (My sixth commit)

---
+++ week-6/practice_10/target/debug/parctice_10.d
@@ -0,0 +1,1 @@
+C:\Users\HomePC\Documents\G.OgbekhiluCOS101\week-6\practice_10\target\debug\parctice_10.exe: C:\Users\HomePC\Documents\G.OgbekhiluCOS101\week-6\practice_10\src\main.rs
---
+++ week-6/practice_4/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let fullname = "Chibudum John Umeh";
+    let department = "Computer Science";
+    let uni = "Pan Atlantic University";
+
+    let mut school = "School of Science".to_string();
+    // push string
+    school.push_str("and Technology");
+
+    println!("My name is:{}",fullname);
+    // check lengt
+    println!("The lengt of my fullname is: {}",fullname.len());
+    println!("I am a student of department: {}",department);
+    println!("{}",school);
+    println!("{}",uni);
+  
+
+
+}
