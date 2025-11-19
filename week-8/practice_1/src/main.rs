3e89e104dbdc449a8b0358106f47e79aebb9de0e
ALLY <godswill.ogbekhilu@pau.edu.ng>
2025-11-19 17:31:56 +0100

    my eight commit

---
+++ week-8/practice_1/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let v : Vec<i64> = Vec::new();
+
+    println!("\nThe lengt of Vec::new is: {}", v.len());
+
+    let v = vec!["Grace","Effiong","Basil","Kareem","Susan"];
+
+    println!("\nTe length of vec macro is: {}", v.len());
+}
---
+++ week-8/practice_10/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+  fn main() {
+    let _b:(i32,bool,f64) = (30, true, 4.9);
+
+}
+fn print(x:(i32,bool,f64)) {
+    println!("Inside print method");
+    let (age,is_male,cpga) = x;
+    println!("Age is {}, is male? {}, cpga is {}",age,is_male,cpga);
+}
+}
